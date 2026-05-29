ec0f034dcc9667c539acaa8558ce00daefdbac03
Hema <mshema19@icloud.com>
2026-05-29 14:41:44 +0800

    Update TechSkills
    Update tech skills

--- js/techStackData.js
+++ js/techStackData.js
@@ -4,12 +4,13 @@
  */
 
 const TECH_STACK_CATEGORIES = [
-  {
-    id: 'operating-systems',
-    title: 'Familiar Operating System (Desktop & Server)',
+{
+    id: 'Computer-Operating-Systems',
+    title: 'Cloud Platforms & Infrastructure Services',
     description: '',
     items: [
-      'Windows (7, 8, 10, Server 2003, etc.)',
+      'Amazon Web Services (AWS), Microsoft Azure, Google Cloud Platform (GCP)',
+      'Windows Server & Desktop (7, 8, 10, Server 2003, etc.)',
       'UNIX (Solaris, Mac OS X, FreeBSD, Fedora)',
       'Linux (Ubuntu, Kali, etc.)'
     ]
@@ -19,20 +20,21 @@ const TECH_STACK_CATEGORIES = [
     title: 'Programming Languages, Databases, Frameworks & Tools',
     description: '',
     items: [
-      'Languages: Java, J2EE, PHP, Python, R, C, Bash, JavaScript',
-      'Web: HTML, HTML5, CSS, Angular.js, React.js, Node.js',
-      'Backend: Django, Flask, Ruby, Ruby on Rails, Spring MVC, Hibernate, Struts',
+      'Programming Languages:Python,Go, Rust, R, C++, Java, Bash, JavaScript',
+      'Frontend Frameworks : React JS, ReactNative, NextJs, AngularJS,Javascripts, JQuery,HTML5',
+      'CSS Frameworks: Bootstrap, Tailwind CSS',
+      'Backend: Django, Flask, FastAPI, Ruby, Ruby on Rails, Java Spring MVC, Hibernate, Struts',
       'Big Data & ML: Hadoop, Pytorch, Tensorflow, Caffe2',
-      'Blockchain: Bitcoin, Ethereum, Lightning Framework, Solidity, Zcash',
+      'Blockchain: Bitcoin,PineScripts, EA for MT5 & MT4, Solana,  Ethereum, Lightning Framework, Solidity, Zcash',
       'Databases: MySQL, MSSQL, SQLite, MongoDB, PostgreSQL, SQLAlchemy',
-      'Mobile: Ionic',
-      'Advanced: OOP, OOAD, Socket Programming, Security Programming, Bootstrap',
-      'Testing: Selenium, Winium, PyAutoGui, AutoGui, UAT, SIT, Unit Testing, Regression Testing, Smoke Testing, Sanity Testing',
+      'Mobile: Flutter',
+      'Programming Pattern , Paradigm : OOP, OOAD, Socket Programming, Security Programming, Bootstrap',
+      'Software Testing Framework & Tools : Selenium, Winium, PyAutoGui, AutoGui, UAT, SIT, Unit Testing, Regression Testing, Smoke Testing, Sanity Testing',
       'Version Control: CVS, Tortoise SVN, Github',
       'IDEs & Tools: Eclipse, PyCharm, IntelliJ, Sublime',
       'Project Management: Agile, Waterfall, Scrum, RAD, Kanban',
-      'CI/CD: Jenkins, Jira',
-      'Other: UML Diagram, Six Sigma'
+      'CI/CD: Jenkins, Jira, Ansible, Puppet, Chef, GitHub Actions',
+      'Other: UML Diagram, Six Sigma, Achimate, TOGAF, ITIL, COBIT, ISO 27001, NIST, OWASP, CISSP, CISA, CEH'
     ]
   },
   {
